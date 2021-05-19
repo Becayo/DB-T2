55a600bbcc64d8309b2815ec010a77fe9a41b547
feliipx <felipe.fuentesvi@usm.cl>
2021-05-19 18:08:23 -0400

    hoal si buenas tardes

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -1,3 +1,9 @@
 <?php
 /* Este archivo debe validar los datos de registro y manejar la lógica de crear un usuario desde el formulario de registro */
+
+    include("db_config.php");
+    session_start();
+
+    $usuario = $_SESSION['email'];
+
 ?>
