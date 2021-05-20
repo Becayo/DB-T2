6ddfc18c03669016fd7fc6cb920a8701c435466e
feliipx <felipe.fuentesvi@usm.cl>
2021-05-20 13:05:49 -0400

    avance en el registro, falta verificar la

--- sesion/valida_login.php
+++ sesion/valida_login.php
@@ -1,4 +1,5 @@
 <?php 
 /* Este archivo debe manejar la lógica de iniciar sesión */
     include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+    
 ?>
--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -3,4 +3,31 @@
 
     include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 
+    if(!dbconn){
+        echo "Error al conectarse a la base de datos";
+    }else{
+        
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            
+            $nombre = pg_escape_string($_POST['name']);
+            $apellido = pg_escape_string($_POST['surname']);
+            $correo = pg_escape_string($_POST['email']);
+            $pass1 = pg_escape_string($_POST['pwd']);
+            $pass2 = pg_escape_string($_POST['pwd2']);
+            $pais = pg_escape_string($_POST['country']);
+            
+            date_default_timezone_set('America/Santiago');
+            
+            $fecha = date("Y-m-d H:i:s");
+            
+            $query = "INSERT INTO usuario(nombre, apellido, email, contraseña, pais, fecha_registro) VALUES('" . $nombre . "', '" . $apellido . "', '" . $correo . "', '" . $pass1 . "', '" . $pais . "', '" . $fecha . "')";
+            
+            if(pg_query_params($dbconn, $query, array($nombre,$apellido,$correo,$pass1,$pass2,$pais)) !== FALSE){
+                echo "Los siguientes datos fueron ingresados con exito ".$correo;
+            }
+        }
+    }
+    pg_close();
+
+
 ?>
