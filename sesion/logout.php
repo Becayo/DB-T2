7f5f97871b1c87bebb6225067914c17609cc2908
feliipx <felipe.fuentesvi@usm.cl>
2021-05-20 11:48:48 -0400

    cambio en los php

--- sesion/logout.php
+++ sesion/logout.php
@@ -1,4 +1,4 @@
 <?php
 /* Este archivo debe manejar la lógica de cerrar una sesión */
-    $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 ?>
--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -1,6 +1,6 @@
 <?php
 /* Este archivo debe validar los datos de registro y manejar la lógica de crear un usuario desde el formulario de registro */
 
-    $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 
 ?>
