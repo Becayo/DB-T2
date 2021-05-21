2e25af8b240145878291bb2a16355988a80b8586
feliipx <felipe.fuentesvi@usm.cl>
2021-05-20 20:13:49 -0400

    se conecta a la bd

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -2,31 +2,27 @@
 /* Este archivo debe validar los datos de registro y manejar la lógica de crear un usuario desde el formulario de registro */
 
     include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
-
-    if(!dbconn){
-        echo "Error al conectarse a la base de datos";
-    }else{
         
-        if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
             
-            $nombre = pg_escape_string($_POST['name']);
-            $apellido = pg_escape_string($_POST['surname']);
-            $correo = pg_escape_string($_POST['email']);
-            $pass1 = pg_escape_string($_POST['pwd']);
-            $pass2 = pg_escape_string($_POST['pwd2']);
-            $pais = pg_escape_string($_POST['country']);
+        $nombre = pg_escape_string($_POST['name']);
+        $apellido = pg_escape_string($_POST['surname']);
+        $correo = pg_escape_string($_POST['email']);
+        $pass1 = pg_escape_string($_POST['pwd']);
+        $pass2 = pg_escape_string($_POST['pwd2']);
+        $pais = pg_escape_string($_POST['country']);
             
-            date_default_timezone_set('America/Santiago');
+        date_default_timezone_set('America/Santiago');
             
-            $fecha = date("Y-m-d H:i:s");
+        $fecha = date("Y-m-d H:i:s");
             
-            $query = "INSERT INTO usuario(nombre, apellido, email, contraseña, pais, fecha_registro) VALUES('" . $nombre . "', '" . $apellido . "', '" . $correo . "', '" . $pass1 . "', '" . $pais . "', '" . $fecha . "')";
+        $query = "INSERT INTO usuario(nombre, apellido, correo, contraseña, pais, fecha_registro) VALUES('" . $nombre . "', '" . $apellido . "', '" . $correo . "', '" . $pass1 . "', '" . $pais . "', '" . $fecha . "')";
             
-            if(pg_query_params($dbconn, $query, array($nombre,$apellido,$correo,$pass1,$pass2,$pais)) !== FALSE){
-                echo "Los siguientes datos fueron ingresados con exito ".$correo;
-            }
+        if(pg_query_params($dbconn, $query, array($nombre,$apellido,$correo,$pass1,$pass2,$pais)) !== FALSE){
+            echo "Los siguientes datos fueron ingresados con exito ".$correo;
         }
     }
+    
     pg_close();
 
 
