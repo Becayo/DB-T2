76287cb057045bba98de0c2d124b11d7c20d2c2b
feliipx <felipe.fuentesvi@usm.cl>
2021-05-25 22:40:44 -0400

    registro casi listo

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -5,23 +5,28 @@
         
     if($_SERVER['REQUEST_METHOD'] == "POST"){
             
-        $nombre = pg_escape_string($_POST['name']);
-        $apellido = pg_escape_string($_POST['surname']);
-        $correo = pg_escape_string($_POST['email']);
-        $pass1 = pg_escape_string($_POST['pwd']);
-        $pass2 = pg_escape_string($_POST['pwd2']);
-        $pais = pg_escape_string($_POST['country']);
+        $nombre = pg_escape_string($dbconn, $_POST['name']);
+        $apellido = pg_escape_string($dbconn, $_POST['surname']);
+        $correo = pg_escape_string($dbconn, $_POST['email']);
+        $pass1 = pg_escape_string($dbconn, $_POST['pwd']);
+        $pass2 = pg_escape_string($dbconn, $_POST['pwd2']);
+        $pais = pg_escape_string($dbconn, $_POST['country']);
+
+
+
+        if(is_null($apellido)){
+            $apellido = NULL;
+        } else{
+            $apellido = pg_escape_string($dbconn, $_POST['surname']);
+        }
             
         date_default_timezone_set('America/Santiago');
             
-        $fecha = pg_escape_string(date("Y-m-d H:i:s", time()));
+        $fecha = pg_escape_string($dbconn, date("Y-m-d H:i:s", time()));
             
-        $query = "INSERT INTO usuario(nombre, apellido, correo, contraseña, pais, fecha_registro) VALUES('" . $nombre . "', '" . $apellido . "', '" . $correo . "', '" . $pass1 . "', '" . $pais . "', '" . $fecha . "')";
+        $query = "INSERT INTO usuario (nombre, apellido, correo, contraseña, pais, fecha_registro) VALUES ('$nombre', '$apellido', '$correo', '$pass1', '$pais', '$fecha')";
             
         pg_query($dbconn, $query);
     }
-    
     pg_close();
-
-
 ?>
