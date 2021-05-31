b3bbd217399c05ae10277c9e2d59580d4f980457
feliipx <felipe.fuentesvi@usm.cl>
2021-05-31 18:35:02 -0400

    registros casi listos

--- sesion/valida_signup.php
+++ sesion/valida_signup.php
@@ -11,22 +11,27 @@
         $pass1 = pg_escape_string($dbconn, $_POST['pwd']);
         $pass2 = pg_escape_string($dbconn, $_POST['pwd2']);
         $pais = pg_escape_string($dbconn, $_POST['country']);
-
-
-
-        if(is_null($apellido)){
-            $apellido = NULL;
-        } else{
-            $apellido = pg_escape_string($dbconn, $_POST['surname']);
-        }
-            
         date_default_timezone_set('America/Santiago');
-            
         $fecha = pg_escape_string($dbconn, date("Y-m-d H:i:s", time()));
+
+        $buscar_usuario = "SELECT * FROM usuario WHERE correo = '$correo'";
+        $resultado = pg_query($dbconn, $buscar_usuario);
+        $contador = pg_num_rows($resultado);
+
+        if($contador != 1){
+
+            if($pass1 == $pass2){
+                $query = "INSERT INTO usuario (nombre, apellido, correo, contraseña, pais, fecha_registro) VALUES ('$nombre', '$apellido', '$correo', '$pass1', '$pais', '$fecha')";
+                pg_query($dbconn, $query);
+                header("location:log-in.html");
+            }else{
+                echo "Las contraseñas no coinciden";
+            }
             
-        $query = "INSERT INTO usuario (nombre, apellido, correo, contraseña, pais, fecha_registro) VALUES ('$nombre', '$apellido', '$correo', '$pass1', '$pais', '$fecha')";
-            
-        pg_query($dbconn, $query);
+        }else{
+            echo "Ya existe un usuario con este correo";
+        }
+        
     }
     pg_close();
 ?>
