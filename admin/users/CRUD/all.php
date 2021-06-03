9c06b3b0b463d456cd265d66ff67ceda8192c474
Gackpod <diego.perezo@usm.cl>
2021-06-03 00:06:20 -0400

    Vista read creada y completada
    Además se completo 100% la vista all, read completo mostrando lo escrito anteriormente. / Separación de php y html

--- admin/users/CRUD/all.php
+++ admin/users/CRUD/all.php
@@ -1,3 +1,5 @@
-<?php
-/* Este archivo debe manejar la lógica de obtener los datos de todos los usuarios */
-?>
+<?php 
+$sqlall="SELECT id,nombre,apellido,correo FROM usuario";
+$result = pg_query ($sqlall) or die('La consulta fallo: ' . pg_last_error()); 
+$line = pg_fetch_all($result)
+?> 
--- admin/users/CRUD/read.php
+++ admin/users/CRUD/read.php
@@ -1,3 +1,10 @@
-<?php
-/* Este archivo debe manejar la lógica de obtener los datos de un determinado usuario */
+<?php $id_actual= $_GET['id'] ;       
+$sql_read =  "SELECT id,nombre,apellido,correo,pais,fecha_registro,contraseña FROM usuario WHERE id=$id_actual";
+$result_read = pg_query ($sql_read) or die('La consulta fallo: ' . pg_last_error());
+$Datos= pg_fetch_array($result_read);
+$pais_actual= $Datos['pais'];
+$fecha_show= substr($Datos['fecha_registro'], 0, -9);
+$sql_pais= "SELECT cod_pais,nombre FROM pais WHERE cod_pais = $pais_actual";
+$result_pais= pg_query ($sql_pais) or die('La consulta fallo: ' . pg_last_error());
+$pais_select= pg_fetch_array($result_pais);
 ?>
