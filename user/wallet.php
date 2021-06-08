cec80ba2da94b9b166d7d79362cf9e88b2954e6c
feliipx <felipe.fuentesvi@usm.cl>
2021-06-08 00:04:47 -0400

    wallet casi lista

--- user/wallet.php
+++ user/wallet.php
@@ -1,3 +1,9 @@
 <?php
-/* Este archivo debe manejar la lógica para obtener la información de la billetera */
+
+$correo = $_SESSION['correo'];
+buscar_usuario = "SELECT * FROM usuario WHERE correo = $correo";
+
+$sqlall="SELECT id,nombre,apellido,correo FROM usuario";
+$result = pg_query ($sqlall) or die('La consulta fallo: ' . pg_last_error()); 
+$line = pg_fetch_all($result)
 ?>
