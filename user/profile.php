36828077507da7a74168a3b982cdee3f2fc26191
feliipx <felipe.fuentesvi@usm.cl>
2021-06-07 20:47:27 -0400

    casi listo los profile

--- user/profile.php
+++ user/profile.php
@@ -1,3 +1,12 @@
 <?php
-/* Este archivo debe manejar la lógica para obtener la información del perfil */
+    include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
+    session_start(); 
+    $sql_read =  "SELECT id,nombre,apellido,correo,pais,fecha_registro,contraseña FROM usuario WHERE correo="."$_SESSION['correo']";
+    $result_read = pg_query ($sql_read) or die('La consulta fallo: ' . pg_last_error());
+    $Datos= pg_fetch_array($result_read);
+    $pais_actual= $Datos['pais'];
+    $fecha_show= substr($Datos['fecha_registro'], 0, -9);
+    $sql_pais= "SELECT cod_pais,nombre FROM pais WHERE cod_pais = $pais_actual";
+    $result_pais= pg_query ($sql_pais) or die('La consulta fallo: ' . pg_last_error());
+    $pais_select= pg_fetch_array($result_pais);
 ?>
