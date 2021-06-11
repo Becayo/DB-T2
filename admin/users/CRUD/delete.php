bcc646b9af2c4d70626d62424f207f7d3ecf31d3
Gackpod <diego.perezo@usm.cl>
2021-06-10 22:15:41 -0400

    Delete on Cascade added
    Tablas modificadas con 'ON DELETE CASCADE' De esa manera al eliminar un usuario, se eliminan las cuentas que tenía el usuario.
    delete.php terminado funcional

--- admin/users/CRUD/delete.php
+++ admin/users/CRUD/delete.php
@@ -1,4 +1,8 @@
 <?php
-$id= $_GET['id'];
+include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
 
+$id= $_GET['id'];
+$sql_delete="DELETE FROM usuario WHERE id=$id";
+$result_delete= pg_query ($sql_delete) or die('La consulta fallo: ' . pg_last_error());
+header("location:../all.html");
 ?>
