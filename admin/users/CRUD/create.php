40135ae1d6f58e43881140f982e5aff5837621d0
Gackpod <diego.perezo@usm.cl>
2021-06-13 16:10:41 -0400

    Comentarios Añadidos
    Archivos comentados:
    all.html all.php create.html create.php read.php update.php update.html
    Ademas de comentarios menores al momento de hacer acciones incorrectas

--- admin/users/CRUD/create.php
+++ admin/users/CRUD/create.php
@@ -2,7 +2,7 @@
     include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
         
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        
+        #Recoger toda la información del forms mediantes POST
         $nombre = pg_escape_string($dbconn, $_POST['name']);
         $apellido = pg_escape_string($dbconn, $_POST['surname']);
         $correo = pg_escape_string($dbconn, $_POST['email']);
@@ -12,11 +12,15 @@
         $fecha = pg_escape_string($dbconn, date("Y-m-d H:i:s", time()));
         $admin = pg_escape_string($dbconn, $_POST['admin']);
 
+        #Revisar cuantas veces aparece el correo en la base
         $buscar_usuario = "SELECT * FROM usuario WHERE correo = '$correo'";
         $resultado = pg_query($dbconn, $buscar_usuario);
         $contador = pg_num_rows($resultado);
 
+        #Si no existe, Se agrega
         if($contador == 0){
+            $pass1=md5($pass1);
+            # If para ingresar valor true o false dependiendo de la opción de admin.
             if ($admin=='2'){
                 $query = "INSERT INTO usuario (nombre, apellido, correo, contraseña, pais, fecha_registro, admin) VALUES ('$nombre', '$apellido', '$correo', '$pass1', '$pais', '$fecha', 'TRUE')";
                 pg_query($dbconn, $query);
@@ -27,7 +31,7 @@
                 header("location:../all.html");
             }
         }else{
-            echo "Ya existe un usuario con este correo";
+            header("location:../create.html?rep_correo");
         }
     }
     pg_close();
--- admin/users/CRUD/update.php
+++ admin/users/CRUD/update.php
@@ -1,27 +1,33 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';
-    include 'read.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/db_config.php';    #Incluir dbconn, la conexión
+    include 'read.php';                                    #Incluir los datos actuales.
 
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){                           #Obtención de todos los datos del formulario
         $nombre = pg_escape_string($dbconn, $_POST['name']);
         $apellido = pg_escape_string($dbconn, $_POST['surname']);
         $correo = pg_escape_string($dbconn, $_POST['email']);
         $pass1 = pg_escape_string($dbconn, $_POST['pwd']);
         $pais = pg_escape_string($dbconn, $_POST['country']);
 
-        $buscar_usuario = "SELECT * FROM usuario WHERE correo = '$correo'";
+        $buscar_usuario = "SELECT * FROM usuario WHERE correo = '$correo'"; #Query para revisar cuantas veces está el correo
         $resultado = pg_query($dbconn, $buscar_usuario);
         $contador = pg_num_rows($resultado);
 
+        #Primer if es para la condición de existencia de 1 solo correo, Si está modificando el propio o está modificando el de alguien
         if(($contador == 1 and $correo==$Datos['correo']) or ($contador == 0 and $correo!=$Datos['correo']) ){
-            $pass1=md5($pass1);
-            $sql_update = 
-            "UPDATE usuario 
-            SET nombre='$nombre',apellido='$apellido',correo='$correo',contraseña='$pass1',pais='$pais' WHERE id=$id_actual";
-            pg_query($dbconn, $sql_update);
-            header("location:../all.html");
+            #Segundo if es para restringir modificar la constraseña de otro admin, esto por si acaso.
+            if (($_SESSION['id']==$id_actual) or ($es_admin==FALSE)){
+                $pass1=md5($pass1);           #Encriptar la contraseña
+                $sql_update =                 #Query update, y luego redirigir
+                "UPDATE usuario 
+                SET nombre='$nombre',apellido='$apellido',correo='$correo',contraseña='$pass1',pais='$pais' WHERE id=$id_actual";
+                pg_query($dbconn, $sql_update);
+                header("location:../all.html?update");      #Redirección + mensaje update
+            }else{
+                header("location:../all.html?mod_admin");   #Redirección + mensaje mod_admin
+            }
         }else{
-            echo "Ya existe un usuario con este correo";
+            header("location:../update.html?id=$id_actual&a=rep_correo"); #Mantener la pagina y mostrar mensaje error por correo repetido
         }
     }
     pg_close();
