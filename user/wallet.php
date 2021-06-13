450e88ac85b011475bd5bb3ee00b387de7f291dc
feliipx <felipe.fuentesvi@usm.cl>
2021-06-13 18:24:56 -0400

    ready <3

--- user/wallet.php
+++ user/wallet.php
@@ -1,15 +1,20 @@
 <?php
-    $id_actual = $_SESSION['id'];
-    $sql_read =  "SELECT id,nombre,apellido,correo,pais,fecha_registro,contraseña,admin FROM usuario WHERE id=$id_actual";
-    $result_read = pg_query ($sql_read) or die('La consulta fallo: ' . pg_last_error());
-    $Datos= pg_fetch_array($result_read);
-    $esadmin = $Datos['admin'];
-
-    if(!isset($_SESSION['id']) || ($esadmin=='t')){
+    
+    if(!isset($_SESSION['id'])){
         header("Location:../index.html");
     }
 
     $id_usuario = $_SESSION['id'];
+    $sql_read =  "SELECT id,nombre,apellido,correo,pais,fecha_registro,contraseña,admin FROM usuario WHERE id=$id_usuario";
+    $result_read = pg_query ($sql_read) or die('La consulta fallo: ' . pg_last_error());
+    $Datos= pg_fetch_array($result_read);
+    $esadmin = $Datos['admin'];
+
+
+    if($esadmin=='t'){
+        header("Location:../index.html");
+    }
+
 
     $sqlall='SELECT sigla, nombre, balance, valor, (balance*valor) AS "total" FROM (SELECT *
         FROM (SELECT * FROM usuario_tiene_moneda WHERE id_usuario = '.$id_usuario.') AS xd JOIN moneda ON moneda.id = xd.id_moneda) AS coin
